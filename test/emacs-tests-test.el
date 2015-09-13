1234e33de497048f921a83cd86721d7cc8478570
papachan <danloaiza@yahoo.com>
2015-09-13 11:41:29 -0500

    committing examples

--- test/emacs-tests-test.el
+++ test/emacs-tests-test.el
@@ -3,3 +3,10 @@
 
 (ert-deftest mytest-t ()
   (should (=  2 (+ 1 1))))
+
+(ert-deftest addition-test ()
+  (should (= (+ 1 2) 3)))
+
+(ert-deftest test-divide-by-zero ()
+  (should-error (/ 1 0)
+                :type 'arith-error))
